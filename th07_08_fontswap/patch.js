f515862e99c628607f108968e1685e7ffaf7e67e
ProjectTojiko <projecttojiko@gmail.com>
2021-04-22 05:34:30 +0900

    Upload

--- th07_08_fontswap/patch.js
+++ th07_08_fontswap/patch.js
@@ -1,1 +1,10 @@
-
+{
+	"dependencies": [
+		"nmlgc/base_tsa"
+	],
+	"id": "th07_08_fontswap",
+	"servers": [
+		"https://raw.githubusercontent.com/ProjectTojiko/thcrap_patches/main/th07_08_fontswap/"
+	],
+	"title": "Alter th07 and th08's system fonts."
+}
