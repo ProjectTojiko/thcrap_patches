28204148245f64baf39e421bdc0655ea03a5f3ab
ProjectTojiko <projecttojiko@gmail.com>
2021-04-22 05:27:30 +0900

    Update patch.js

--- seEoSD/patch.js
+++ seEoSD/patch.js
@@ -1,1 +1,10 @@
-
+{
+	"dependencies": [
+		"nmlgc/base_tsa"
+	],
+	"id": "seEoSD",
+	"servers": [
+		"https://raw.githubusercontent.com/ProjectTojiko/thcrap_patches/main/Misc_BugPatch/"
+	],
+	"title": "(Beta version) Changes some of the sound effects into EoSD style."
+}
