9fd5d7cc598a6ad7ab77654861f5f492a7f101f8
ProjectTojiko <projecttojiko@gmail.com>
2021-04-22 05:53:36 +0900

    Create repo.js

---
+++ repo.js
@@ -0,0 +1,14 @@
+{
+	"contact": "projecttojiko@gmail.com",
+	"id": "ProjectTojiko",
+	"patches": {
+		"classic_powerup_se": "(th13 and later) Changes Powerup sound to the classic pre-th12 sound.",
+		"seEoSD": "(Beta version) Changes some of the sound effects into EoSD style.",
+		"th06_07_08_fontswap": "Alter th06/th07 and th08's system fonts.",
+		"Misc_BugPatch": "(Currently th13 and th14) Fixes miscellaneous bugs and glitches.",
+	},
+	"servers": [
+		"https://raw.githubusercontent.com/ProjectTojiko/thcrap_patches/main/"
+	],
+	"title": "Tojiko's Miscellaneous Patches"
+}
