1f83252092e7e166949c80a94ff4b368fa58a903
ProjectTojiko <projecttojiko@gmail.com>
2021-04-22 05:23:17 +0900

    Update patch.js

--- classic_powerup_se/patch.js
+++ classic_powerup_se/patch.js
@@ -1,1 +1,10 @@
-
+{
+	"dependencies": [
+		"nmlgc/base_tsa"
+	],
+	"id": "classic_power_se",
+	"servers": [
+		"https://raw.githubusercontent.com/ProjectTojiko/thcrap_patches/main/classic_powerup_se/"
+	],
+	"title": "(th13 and later) Changes Powerup sound to the classic pre-th12 sound."
+}
