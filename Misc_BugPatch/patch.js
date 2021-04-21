9491b82e468dd6cf46d4e331d6a11d1933077f53
ProjectTojiko <projecttojiko@gmail.com>
2021-04-22 05:24:58 +0900

    Create patch.js

---
+++ Misc_BugPatch/patch.js
@@ -0,0 +1,10 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa"
+	],
+	"id": "Misc_BugPatch",
+	"servers": [
+		"https://raw.githubusercontent.com/ProjectTojiko/thcrap_patches/main/Misc_BugPatch/"
+	],
+	"title": "(Currently th13 and th14) Fixes miscellaneous bugs and glitches."
+}
