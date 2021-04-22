9ff39aad9b06a75e0b1442e0a879cc67567e3a3b
ProjectTojiko <projecttojiko@gmail.com>
2021-04-22 13:37:52 +0900

    Beta version complete

--- fontswap/files.js
+++ fontswap/files.js
@@ -1,9 +1,10 @@
 {
-	"patch.js": 157755783,
+	"patch.js": 735895210,
 	"th06/ascii.png": 594629107,
 	"th06/asciis.png": 326907963,
 	"th07/data/ascii/ascii.png": 1036509312,
 	"th07/data/ascii/asciis.png": 3489721045,
 	"th08/data/ascii/ascii.png": 3778998103,
-	"th08/data/ascii/asciis.png": 1860777919
+	"th08/data/ascii/asciis.png": 1860777919,
+	"th10/front/front00.png": 1146539505
 }
