286ccf3eebbeacc043c6425df03cfc07ca3c15ca
ProjectTojiko <82974133+ProjectTojiko@users.noreply.github.com>
2021-04-22 13:53:15 +0900

    Update repo.js

--- repo.js
+++ repo.js
@@ -4,7 +4,7 @@
 	"patches": {
 		"Misc_BugPatch": "(Currently th13 and th14) Fixes miscellaneous bugs and glitches.",
 		"classic_powerup_se": "(th13 and later) Changes Powerup sound to the classic pre-th12 sound.",
-		"fontswap": "Alters th06/th07 and th08's system fonts. th10's score fonts change into those of th11's and onwards.",
+		"fontswap": "Alters th06/th07 and th08 system fonts. th10 score fonts change into those of th11 and onwards.",
 		"seEoSD": "(Beta version) Changes some of the sound effects into EoSD style."
 	},
 	"servers": [
