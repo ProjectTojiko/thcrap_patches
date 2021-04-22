f971504604e99c1aedf669ec9297599b2612f7c6
ProjectTojiko <82974133+ProjectTojiko@users.noreply.github.com>
2021-04-22 09:20:46 +0900

    Update patch.js

--- th06_07_08_fontswap/patch.js
+++ th06_07_08_fontswap/patch.js
@@ -6,5 +6,5 @@
 	"servers": [
 		"https://raw.githubusercontent.com/ProjectTojiko/thcrap_patches/master/th06_07_08_fontswap/"
 	],
-	"title": "Alter th06/th07 and th08's system fonts. th10's score fonts become that of th11's and later's."
+	"title": "Alter th06/th07 and th08's system fonts. th10's score fonts become that of th11's and later's too."
 }
