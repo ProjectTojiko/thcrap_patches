447a0aec4205fa50e4e8ddca269b84037bb8a5bf
ProjectTojiko <projecttojiko@gmail.com>
2021-04-23 03:31:01 +0900

    Cleaner asciis.png
    th08's 'Bonus' and 'history' removed due to lack of space.

--- fontswap/files.js
+++ fontswap/files.js
@@ -1,10 +1,10 @@
 {
 	"patch.js": 735895210,
 	"th06/ascii.png": 594629107,
-	"th06/asciis.png": 326907963,
+	"th06/asciis.png": 273346188,
 	"th07/data/ascii/ascii.png": 1036509312,
-	"th07/data/ascii/asciis.png": 3489721045,
-	"th08/data/ascii/ascii.png": 3778998103,
+	"th07/data/ascii/asciis.png": 2507286908,
+	"th08/data/ascii/ascii.png": 1765273937,
 	"th08/data/ascii/asciis.png": 1860777919,
 	"th10/front/front00.png": 1146539505
 }
