231a410e7552a23459156e08da42c4e6b671028d
ProjectTojiko <projecttojiko@gmail.com>
2021-05-22 01:59:57 +0900

    Disabled fontswap and new_bonus_hist.

--- bgmloop_fix/patch.js
+++ bgmloop_fix/patch.js
@@ -6,5 +6,5 @@
 	"servers": [
 		"https://raw.githubusercontent.com/ProjectTojiko/thcrap_patches/master/bgmloop_fix/"
 	],
-	"title": "Fixes th13_20.wav and th14_10.wav's BGM loop point."
+	"title": "Fixes th13_20.wav and th14_11.wav's BGM loop point."
 }
