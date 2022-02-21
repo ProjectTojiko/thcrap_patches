734d24b71a1e674c875c68ca903e76f28bf032c8
ProjectTojiko <projecttojiko@gmail.com>
2022-02-22 04:52:51 +0900

    Added sanaeb_tp patch info

---
+++ sanaeb_tp/files.js
@@ -0,0 +1,7 @@
+{
+	"patch.js": 3986963937,
+	"th12/player/pl02/pl02.png": 2703247389,
+	"th15/player/pl02/pl02.png": 2829393261,
+	"th15/player/pl02/pl02b.png": 2829393261,
+	"th18/player/pl03/pl03.png": 2829393261
+}
---
+++ sanaeb_tp/patch.js
@@ -0,0 +1,10 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa"
+	],
+	"id": "sanaeb_tp",
+	"servers": [
+		"https://raw.githubusercontent.com/ProjectTojiko/thcrap_patches/master/sanaeb_tp/"
+	],
+	"title": "(th12, 15, 18) Lowers Sanae's Frog missiles' opacity to make them less distracting."
+}
