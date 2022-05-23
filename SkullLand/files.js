7b8ffac406e9fd0a8bdcdbb7d509925fa4f8c186
ProjectTojiko <projecttojiko@gmail.com>
2022-05-23 23:17:48 +0900

    More files.

--- SkullLand/files.js
+++ SkullLand/files.js
@@ -1,7 +1,11 @@
 {
 	"patch.js": 668193605,
 	"th14/background/stage01/world02b.png": 448592605,
+	"th14/background/stage02/stg3bg3.png": 448592605,
 	"th14/background/stage03/stg3bg1.png": 448592605,
+	"th14/background/stage04/stg4bg1.png": 448592605,
+	"th14/background/stage05/stage05a.png": 448592605,
+	"th14/background/stage06/stage05a.png": 448592605,
 	"th15/background/stage01/st01a.png": 448592605,
 	"th15/background/stage02/st02a.png": 448592605,
 	"th15/background/stage03/stg3bg1.png": 448592605,
