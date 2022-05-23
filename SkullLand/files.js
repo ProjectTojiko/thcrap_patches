be06673681910f767fc0eeb7e78854672a8d8dd9
ProjectTojiko <projecttojiko@gmail.com>
2022-05-23 17:56:16 +0900

    Added eff_switchbg.png

--- SkullLand/files.js
+++ SkullLand/files.js
@@ -1,3 +1,8 @@
 {
-	"patch.js": 2659130046
+	"patch.js": 2659130046,
+	"th15/effect/eff_switchbg.png": 448592605,
+	"th16/effect/eff_switchbg.png": 448592605,
+	"th165/effect/eff_switchbg.png": 448592605,
+	"th17/effect/eff_switchbg.png": 448592605,
+	"th18/effect/eff_switchbg.png": 448592605
 }
