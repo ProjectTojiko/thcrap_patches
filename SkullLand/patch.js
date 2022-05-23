957e3fbdd1e3005b99b5c3ff6c9a3b7c0bb571c8
ProjectTojiko <projecttojiko@gmail.com>
2022-05-23 15:08:53 +0900

    SkullLand

---
+++ SkullLand/patch.js
@@ -0,0 +1,10 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa"
+	],
+	"id": "SkullLand",
+	"servers": [
+		"https://raw.githubusercontent.com/ProjectTojiko/thcrap_patches/master/SkullLand/"
+	],
+	"title": "Changes stage backgrounds into skulls!"
+}
