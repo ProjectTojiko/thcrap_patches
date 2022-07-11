de9611a48cd876ea2d17ad9c213a8e0fee74c464
ProjectTojiko <projecttojiko@gmail.com>
2022-07-11 23:05:56 +0900

    Changed the name of the patch.

---
+++ shot_effect_opacity/patch.js
@@ -0,0 +1,11 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa"
+	],
+	"id": "shot_effect_opacity",
+	"servers": [
+		"https://raw.githubusercontent.com/ProjectTojiko/thcrap_patches/master/shot_effect_opacity/"
+	],
+	"title": "(th11-15, th18) Marisa's Magic Missile and Sanae's Cobalt Spread effects' opacity lowered to make them less distracting.",
+	"update": true
+}
