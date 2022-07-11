75b6a690b7cb5f4a707ab39330c01f4a6bf021b3
ProjectTojiko <projecttojiko@gmail.com>
2022-07-11 19:56:38 +0900

    I think I should change the name of the patch...

--- sanaeb_tp/files.js
+++ sanaeb_tp/files.js
@@ -1,6 +1,11 @@
 {
 	"patch.js": 2864573771,
-	"th12/player/pl02/pl02.png": 4101952046,
+	"th11/player/pl01/pl01.png": 3105563217,
+	"th12/player/pl02/pl02.png": 2210887789,
+	"th13/player/pl01/pl01.png": 1437003108,
+	"th14/player/pl01/pl01.png": 3474694401,
+	"th14/player/pl01/pl01f.png": 911957170,
+	"th15/player/pl01/pl01.png": 3474694401,
 	"th15/player/pl02/pl02.png": 2829393261,
 	"th15/player/pl02/pl02b.png": 2829393261,
 	"th18/player/pl01/pl01.png": 648189471,
