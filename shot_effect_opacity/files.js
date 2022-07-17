fe1913bcb32959bf28c3881eee2934ce78ee3af0
ProjectTojiko <projecttojiko@gmail.com>
2022-07-18 02:28:18 +0900

    bgmloop_fix: Changed patch name into bgm_looppoint_fix.

--- shot_effect_opacity/files.js
+++ shot_effect_opacity/files.js
@@ -8,6 +8,6 @@
 	"th15/player/pl01/pl01.png": 1151444565,
 	"th15/player/pl02/pl02.png": 921313262,
 	"th15/player/pl02/pl02b.png": 921313262,
-	"th18/player/pl01/pl01.png": 648189471,
+	"th18/player/pl01/pl01.png": 909379494,
 	"th18/player/pl03/pl03.png": 921313262
 }
