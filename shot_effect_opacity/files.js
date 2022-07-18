57defa2b73c12b463e7e66b30b0d16f66892aeb1
ProjectTojiko <projecttojiko@gmail.com>
2022-07-18 19:34:26 +0900

    Changed description.

--- shot_effect_opacity/files.js
+++ shot_effect_opacity/files.js
@@ -1,13 +1,13 @@
 {
-	"patch.js": 3305312743,
+	"patch.js": 2038266050,
 	"th11/player/pl01/pl01.png": 3105563217,
-	"th12/player/pl02/pl02.png": 2646251354,
+	"th12/player/pl02/pl02.png": 2473096566,
 	"th13/player/pl01/pl01.png": 2844773831,
 	"th14/player/pl01/pl01.png": 1151444565,
 	"th14/player/pl01/pl01f.png": 911957170,
 	"th15/player/pl01/pl01.png": 1151444565,
 	"th15/player/pl02/pl02.png": 921313262,
 	"th15/player/pl02/pl02b.png": 921313262,
-	"th18/player/pl01/pl01.png": 909379494,
+	"th18/player/pl01/pl01.png": 21305806,
 	"th18/player/pl03/pl03.png": 921313262
 }
--- shot_effect_opacity/patch.js
+++ shot_effect_opacity/patch.js
@@ -6,6 +6,6 @@
 	"servers": [
 		"https://raw.githubusercontent.com/ProjectTojiko/thcrap_patches/master/shot_effect_opacity/"
 	],
-	"title": "(th11-15, th18) Marisa's Magic Missile and Sanae's Cobalt Spread effects' opacity lowered to make them less distracting.",
+	"title": "(th11-15, th18) Marisa's Magic Missile and Sanae's Cobalt Spread effects' opacity lowered (to not-so-distracting level).",
 	"update": true
 }
