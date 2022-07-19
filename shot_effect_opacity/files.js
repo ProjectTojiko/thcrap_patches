d44e5e7348af6168c3660ff47093a65347aa6086
ProjectTojiko <projecttojiko@gmail.com>
2022-07-19 12:40:09 +0900

    Trials & Errors

--- shot_effect_opacity/files.js
+++ shot_effect_opacity/files.js
@@ -6,9 +6,9 @@
 	"th14/player/pl01/pl01.png": 1151444565,
 	"th14/player/pl01/pl01f.png": 911957170,
 	"th15/player/pl01/pl01.png": 1151444565,
-	"th15/player/pl02/pl02.png": 921313262,
-	"th15/player/pl02/pl02b.png": 921313262,
+	"th15/player/pl02/pl02.png": 3340853976,
+	"th15/player/pl02/pl02b.png": 3340853976,
 	"th18/player/pl01/pl01.png": 3263376309,
-	"th18/player/pl03/pl03.png": 921313262,
+	"th18/player/pl03/pl03.png": 3340853976,
 	"th18/player/pl03/pl03c.png": 3169511353
 }
