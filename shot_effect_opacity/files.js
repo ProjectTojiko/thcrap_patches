5dfc6134ec5baa49c678fec86a3f764a4b8e1752
ProjectTojiko <projecttojiko@gmail.com>
2022-07-27 06:38:53 +0900

    The splash effect should be recognizable with the bright background.

--- shot_effect_opacity/files.js
+++ shot_effect_opacity/files.js
@@ -1,7 +1,7 @@
 {
 	"patch.js": 164171710,
 	"th11/player/pl01/pl01.png": 2282654736,
-	"th12/player/pl02/pl02.png": 2881132115,
+	"th12/player/pl02/pl02.png": 2185501730,
 	"th13/player/pl01/pl01.png": 2986453565,
 	"th14/player/pl01/pl01.png": 1152802882,
 	"th14/player/pl01/pl01f.png": 911957170,
