bba5fca6dba72f51f013b12ef9bd1d68289660b4
ProjectTojiko <projecttojiko@gmail.com>
2022-08-02 06:31:48 +0900

    This patch is not finished. MORE Trials & Errors are ahead.

--- shot_effect_opacity/files.js
+++ shot_effect_opacity/files.js
@@ -1,14 +1,14 @@
 {
 	"patch.js": 164171710,
 	"th11/player/pl01/pl01.png": 2282654736,
-	"th12/player/pl02/pl02.png": 2185501730,
-	"th13/player/pl01/pl01.png": 2986453565,
-	"th14/player/pl01/pl01.png": 1152802882,
+	"th12/player/pl02/pl02.png": 2881132115,
+	"th13/player/pl01/pl01.png": 2316014056,
+	"th14/player/pl01/pl01.png": 1410189268,
 	"th14/player/pl01/pl01f.png": 911957170,
-	"th15/player/pl01/pl01.png": 1152802882,
+	"th15/player/pl01/pl01.png": 1410189268,
 	"th15/player/pl02/pl02.png": 2653740285,
 	"th15/player/pl02/pl02b.png": 2653740285,
-	"th18/player/pl01/pl01.png": 3723492232,
+	"th18/player/pl01/pl01.png": 1414869170,
 	"th18/player/pl03/pl03.png": 2653740285,
 	"th18/player/pl03/pl03c.png": 3169511353
 }
