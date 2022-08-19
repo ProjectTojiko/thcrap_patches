2f0a4eb709df5f3e254705515d7c0fff2158c8d8
ProjectTojiko <projecttojiko@gmail.com>
2022-08-20 06:47:22 +0900

    Sanae's snakes

--- shot_effect_opacity/files.js
+++ shot_effect_opacity/files.js
@@ -3,7 +3,7 @@
 	"th10/player/pl00/pl00.png": 3603418491,
 	"th11/player/pl01/pl01.png": 2282654736,
 	"th12/player/pl00/pl00.png": 3130308967,
-	"th12/player/pl02/pl02.png": 2881132115,
+	"th12/player/pl02/pl02.png": 55006136,
 	"th13/player/pl00/pl00.png": 3480817568,
 	"th13/player/pl01/pl01.png": 306764398,
 	"th14/player/pl00/pl00.png": 3480817568,
@@ -11,10 +11,10 @@
 	"th14/player/pl01/pl01f.png": 911957170,
 	"th15/player/pl00/pl00.png": 2800719509,
 	"th15/player/pl01/pl01.png": 1904814910,
-	"th15/player/pl02/pl02.png": 2653740285,
-	"th15/player/pl02/pl02b.png": 2653740285,
+	"th15/player/pl02/pl02.png": 4025809421,
+	"th15/player/pl02/pl02b.png": 4025809421,
 	"th18/player/pl00/pl00.png": 1514431471,
 	"th18/player/pl01/pl01.png": 2353933421,
-	"th18/player/pl03/pl03.png": 2653740285,
+	"th18/player/pl03/pl03.png": 3178651771,
 	"th18/player/pl03/pl03c.png": 3169511353
 }
