8707b878447ccd0b2a91f692596e429a2dcb63a4
ProjectTojiko <projecttojiko@gmail.com>
2022-08-29 22:14:38 +0900

    Changed patch description and added some effects.

--- shot_effect_opacity/patch.js
+++ shot_effect_opacity/patch.js
@@ -6,6 +6,6 @@
 	"servers": [
 		"https://raw.githubusercontent.com/ProjectTojiko/thcrap_patches/master/shot_effect_opacity/"
 	],
-	"title": "(WIP/th10-15, 18) Some shot effects' opacity lowered (to not-so-distracting level) (ex: Marisa's Magic Missile and Sanae's Cobalt Spread).",
+	"title": "(WIP/th10-16, 18) Some shot effects' opacity lowered (to not-so-distracting level) (ex: Marisa's Magic Missile and Sanae's Cobalt Spread explosion).",
 	"update": true
 }
