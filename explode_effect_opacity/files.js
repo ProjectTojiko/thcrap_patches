05b056367b95f8820a94d104aa4b5754465feabd
ProjectTojiko <projecttojiko@gmail.com>
2023-03-24 06:24:29 +0900

    New patch, but only test build.

---
+++ explode_effect_opacity/files.js
@@ -0,0 +1,21 @@
+{
+	"patch.js": 4041779133,
+	"th07/etama2.png": 2171084821,
+	"th08/etama2.png": 2171084821,
+	"th09/etama2.png": 2231630392,
+	"th095/other/etama2.png": 1746167188,
+	"th10/bullet/etama2.png": 2679363594,
+	"th11/bullet/etama2.png": 2679363594,
+	"th12/bullet/etama2.png": 2679363594,
+	"th125/bullet/etama2.png": 2679363594,
+	"th128/bullet/etama2.png": 2679363594,
+	"th13/effect/eff_base.png": 2679363594,
+	"th14/effect/eff_deadcircle.png": 3523037544,
+	"th143/effect/eff_deadcircle.png": 3523037544,
+	"th15/effect/eff_deadcircle.png": 3523037544,
+	"th16/effect/eff_deadcircle.png": 3523037544,
+	"th165/effect/eff_deadcircle.png": 3523037544,
+	"th17/effect/eff_deadcircle.png": 3523037544,
+	"th18/effect/eff_deadcircle.png": 3523037544,
+	"th185/effect/eff_deadcircle.png": 3523037544
+}
---
+++ explode_effect_opacity/patch.js
@@ -0,0 +1,11 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa"
+	],
+	"id": "explode_effect_opacity",
+	"servers": [
+		"https://raw.githubusercontent.com/ProjectTojiko/thcrap_patches/master/explode_effect_opacity/"
+	],
+	"title": "(Testing stage) Changes the opacity of eff_deadcircle (enemy explosion effect).",
+	"update": true
+}
