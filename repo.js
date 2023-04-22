adc6288f8742bb82143ae0aa5aeae5d9940bdea7
ProjectTojiko <projecttojiko@gmail.com>
2023-04-23 06:21:46 +0900

    And here comes a new patch.

--- repo.js
+++ repo.js
@@ -6,6 +6,7 @@
 		"bgm_looppoint_fix": "Fixes improperly set BGM loop points (th13 Stage 5 boss theme, th14 Stage 5 theme).",
 		"classic_pwup_se": "(th13-) Changes Powerup sound to the classic pre-th12 sound.",
 		"explode_effect_opacity": "(Testing stage) Changes the opacity of eff_deadcircle (enemy explosion effect).",
+		"knives_from_alcostg": "(Testing stage) Changes the knife bullet graphics into that of Uwabami Breakers (a.k.a alcostg).",
 		"seEoSD": "(th07-) Changes most of the sound effects into EoSD style (if that SE existed in EoSD). Note that se_kira00.wav may not be properly heard.",
 		"shot_effect_opacity": "(Beta, WIP/th10-16, 18) Some shot effects' opacity lowered (to not-so-distracting level) (ex: Marisa's Magic Missile and Sanae's Cobalt Spread explosion).",
 		"th185_Bule": "(th185) (Temporary patch) Fixes the 'FairyWave Rainy Bule' typo."
